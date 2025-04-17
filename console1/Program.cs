b85f908fc06aa76fbb5cef641d3204ffd851a780
emineglr <emine.drms.45@gmail.com>
2025-04-17 12:43:58 +0300

    first commit

---
+++ console1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i=1;
+while (true)
+{
+    Console.Write(i + ". sayıyı giriniz(çıkmak için 0 a basınız) :");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    if(sayi>0 && sayi %2 == 0)
+    {
+        Console.WriteLine(sayi + " çift sayıdır");
+    }
+    else if ( sayi == 0)
+        break;
+    i++;
+} 
+    
+
