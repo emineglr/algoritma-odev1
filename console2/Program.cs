2f638534b9bd3a770efcb2ad9f3802243e3f4101
emineglr <emine.drms.45@gmail.com>
2025-04-17 15:25:46 +0300

    multiple projects

--- console2/Program.cs
+++ console2/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+int m = 0;
+int n = 0;
+while (n <= 0)
+{
+    Console.Write("n sayısını giriniz : ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+while (m <= 0)
+{
+    Console.Write("m sayısını giriniz : ");
+    m = Convert.ToInt32(Console.ReadLine());
+}
+
+int sayi;
+for (int i = 1; i <= n; i++)
+{
+    Console.Write(i + ". sayıyı giriniz:");
+    sayi = Convert.ToInt32(Console.ReadLine());
+    if (sayi == m || sayi % m == 0)
+        Console.WriteLine(sayi);
+}
+
